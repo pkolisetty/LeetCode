4fad73ad79a3c6ac24a1a40b09fb27de1c3ec045
pkolisetty <67064651+pkolisetty@users.noreply.github.com>
2022-02-12 15:31:52 -0500

    Time: 1 ms (87.81%), Space: 42.7 MB (13.94%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        
+        List<String> str = new ArrayList<>();
+        
+        if(digits.length() == 0) {
+            return str;
+        }
+        
+        String[] chars = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        
+        int firstDigitValue = Character.getNumericValue(digits.charAt(0));
+        String firstDigitString = chars[firstDigitValue];
+        
+        //adding first digit
+        for(int i = 0; i < firstDigitString.length(); i++){
+            str.add(Character.toString(firstDigitString.charAt(i)));
+        }
+        
+        
+        for(int i = 1; i < digits.length(); i++){
+            int size = str.size();
+            int digit = Character.getNumericValue(digits.charAt(i));
+            String digitString = chars[digit];
+            
+            for(int j = 0; j < size; j++){
+                String current = str.get(j);
+                
+                for(int k = 0; k < digitString.length(); k++){
+                    str.add(current + Character.toString(digitString.charAt(k)));
+                } 
+            }
+            
+            for(int j = 0; j < size; j++){
+                str.remove(0);
+            }
+        }
+        return str;
+    }
+}
