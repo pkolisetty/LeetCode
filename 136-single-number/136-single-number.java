d0264eb81fc95b6f79eb888b7aaa418b0a3fdef6
pkolisetty <67064651+pkolisetty@users.noreply.github.com>
2022-02-15 19:27:33 -0500

    Time: 1 ms (95.85%), Space: 42.4 MB (69.07%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,15 +1,19 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
+//         Set<Integer> set = new HashSet<>();
         
-        for(int i : nums){
-            if(set.contains(i)){
-                set.remove(i);
-            } else {
-                set.add(i);
-            }
-        }
+//         for(int i : nums){
+//             if(set.contains(i)){
+//                 set.remove(i);
+//             } else {
+//                 set.add(i);
+//             }
+//         }
         
-        return set.iterator().next();
+//         return set.iterator().next();
+        int res = 0;
+        for(int i : nums)
+            res ^= i;
+        return res;
     }
 }
